42fd363b70605cc81cc09d4877e5a053b4815096
crackhex <troike@arizona.edu>
2024-10-08 18:56:29 -0700

    add test for trig_table

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #![feature(integer_sign_cast)]
 
 mod includes;
+mod tests;
 use includes::mario_state::MarioState;
-
 
 fn main() {
     let mut mario_state = MarioState::new();
---
+++ src/tests.rs
@@ -0,0 +1,16 @@
+use crate::includes::trig_table::coss;
+use std::time::Instant;
+#[test]
+fn test_trig() {
+    let time = Instant::now();
+    let mut array: [f32; u16::MAX as usize + 1] = [0.0; 65536];
+    let x = (u16::MIN..u16::MAX).into_iter().for_each(
+        |i| {
+            array[i as usize] = coss(i);
+
+        }
+    );
+    println!("{:?}, {:?}", &array, &time.elapsed())
+}
+
+
