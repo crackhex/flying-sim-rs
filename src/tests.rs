edb9a33dc092a433746aae955522241b03f06945
crackhex <troike@arizona.edu>
2024-10-19 00:26:18 -0700

    re-add imports for tests.rs for compiler

--- src/tests.rs
+++ src/tests.rs
@@ -1,13 +1,16 @@
+use std::time::Instant;
+use crate::includes::trig_table::coss;
+
 #[test]
 fn test_trig() {
     let time = Instant::now();
     let mut array: [f32; u16::MAX as usize + 1] = [0.0; 65536];
-    let _x = (u16::MIN..u16::MAX).into_iter().for_each(
+    let _x = (i16::MIN..i16::MAX).into_iter().for_each(
         |i| {
             array[i as usize] = coss(i);
         }
     );
-    //println!("{:?}, {:?}", &array, &time.elapsed())
+    println!("{:?}, {:?}", &array, &time.elapsed())
 }
 
 
