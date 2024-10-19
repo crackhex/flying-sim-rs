d7d720c0013252e68d264ee958006d4756f19be7
crackhex <troike@arizona.edu>
2024-10-19 00:29:19 -0700

    fix the test

--- src/tests.rs
+++ src/tests.rs
@@ -7,7 +7,7 @@ fn test_trig() {
     let mut array: [f32; u16::MAX as usize + 1] = [0.0; 65536];
     let _x = (i16::MIN..i16::MAX).into_iter().for_each(
         |i| {
-            array[i as usize] = coss(i);
+            array[i.cast_unsigned() as usize] = coss(i);
         }
     );
     println!("{:?}, {:?}", &array, &time.elapsed())
